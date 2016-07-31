36d62d71f0071a032d50980300be5940d81a1c3f
ianrahman <ian.alexander.rahman@gmail.com>
2016-07-31 19:38:01 -0400

    Added asynchronicity

--- swift-multithreading-lab/ImageViewController.swift
+++ swift-multithreading-lab/ImageViewController.swift
@@ -13,18 +13,37 @@ import CoreImage
 class ImageViewController : UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Start the activity indicator
+        print("Starting activity indicator")
+        activityIndicator.startAnimating()
+        
+        // Create the image and tell the activity indicator to stop after the image has been created
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { [unowned self] in
+            self.createImage { (result) in
+                dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                    if result { self.activityIndicator.stopAnimating(); print("Stopping activity indicator") }
+                }
+            }
+        }
+    }
+    
+    func createImage(completion: (Bool) -> ()) {
+        
+        print("Creating image")
+        
         guard let image = imageView?.image, cgimg = image.CGImage else {
             print("imageView doesn't have an image!")
+            completion(false)
             return
         }
         
         let openGLContext = EAGLContext(API: .OpenGLES2)
         let context = CIContext(EAGLContext: openGLContext!)
-        
         let coreImage = CIImage(CGImage: cgimg)
         
         let sepiaFilter = CIFilter(name: "CISepiaTone")
@@ -35,15 +54,22 @@ class ImageViewController : UIViewController {
             let exposureFilter = CIFilter(name: "CIExposureAdjust")
             exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
             exposureFilter?.setValue(1, forKey: kCIInputEVKey)
+            print("CISepiaTone applied")
             
             if let exposureOutput = exposureFilter?.valueForKey(kCIOutputImageKey) as? CIImage {
                 let output = context.createCGImage(exposureOutput, fromRect: exposureOutput.extent)
                 let result = UIImage(CGImage: output)
-                imageView?.image = result
+                print("CIExposureAdjust applied")
+
+                dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                    print("Setting the new image")
+                    self.imageView?.image = result
+                }
+                
+                print("Returning completion from createImage")
+                completion(true)
             }
         }
         
     }
-    
-    
 }
