28320d2fdc1deb12df68b0bc3b3d46af0d931771
ianrahman <ian.alexander.rahman@gmail.com>
2016-08-02 11:05:43 -0400

    Completed solution

--- swift-multithreading-lab/ImageViewController.swift
+++ swift-multithreading-lab/ImageViewController.swift
@@ -18,50 +18,59 @@ class ImageViewController : UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imageView = UIImageView(image: UIImage(named: "FlatironFam"))
-        
-        scrollView = UIScrollView(frame: view.bounds)
-        scrollView.backgroundColor = UIColor.blackColor()
-        scrollView.contentSize = imageView.bounds.size
-        scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        scrollView.contentOffset = CGPoint(x: 800, y: 200)
-        
-        scrollView.addSubview(imageView)
-        view.addSubview(scrollView)
-        
-        scrollView.delegate = self
-        
-        activityIndicator = UIActivityIndicatorView()
-        activityIndicator.color = UIColor.cyanColor()
-        
-        view.addSubview(activityIndicator)
-        
-        setZoomScale()
-        
-        setupGestureRecognizer()
+        setupViews()
     }
     
     @IBAction func antiqueButton(sender: AnyObject) {
         print("Starting activity indicator")
         activityIndicator.startAnimating()
-//        self.filterImage { (result) in
-//            if result {
-//                self.activityIndicator.stopAnimating()
-//            }
-//        }
-                let userQueue = NSOperationQueue()
-                userQueue.qualityOfService = .UserInitiated
-                userQueue.addOperationWithBlock {
-                    self.filterImage { (result) in
-                        NSOperationQueue.mainQueue().addOperationWithBlock {
-                            if result {
-                                self.activityIndicator.stopAnimating()
-                                print("Stopping activity indicator")
-                            }
-                        }
+        let userQueue = NSOperationQueue()
+        userQueue.qualityOfService = .UserInitiated
+        userQueue.addOperationWithBlock {
+            self.filterImage { (result) in
+                NSOperationQueue.mainQueue().addOperationWithBlock {
+                    if result {
+                        self.activityIndicator.stopAnimating()
+                        print("Stopping activity indicator")
                     }
                 }
+            }
+        }
+    }
+    
+    func filterImage(completion: (Bool) -> ()) {
+        guard let image = imageView?.image, cgimg = image.CGImage else {
+            print("imageView doesn't have an image!")
+            completion(false)
+            return
+        }
+        
+        let openGLContext = EAGLContext(API: .OpenGLES2)
+        let context = CIContext(EAGLContext: openGLContext!)
+        let coreImage = CIImage(CGImage: cgimg)
+        
+        let sepiaFilter = CIFilter(name: "CISepiaTone")
+        sepiaFilter?.setValue(coreImage, forKey: kCIInputImageKey)
+        sepiaFilter?.setValue(1, forKey: kCIInputIntensityKey)
+        print("Applying CISepiaTone")
+        
+        if let sepiaOutput = sepiaFilter?.valueForKey(kCIOutputImageKey) as? CIImage {
+            let exposureFilter = CIFilter(name: "CIExposureAdjust")
+            exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
+            exposureFilter?.setValue(1, forKey: kCIInputEVKey)
+            print("Applying CIExposureAdjust")
+            
+            if let exposureOutput = exposureFilter?.valueForKey(kCIOutputImageKey) as? CIImage {
+                let output = context.createCGImage(exposureOutput, fromRect: exposureOutput.extent)
+                let result = UIImage(CGImage: output)
+                
+                NSOperationQueue.mainQueue().addOperationWithBlock({
+                    print("Rendering image")
+                    self.imageView?.image = result
+                    completion(true)
+                })
+            }
+        }
     }
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
@@ -99,7 +108,6 @@ class ImageViewController : UIViewController, UIScrollViewDelegate {
     }
     
     func handleDoubleTap(recognizer: UITapGestureRecognizer) {
-        
         if (scrollView.zoomScale > scrollView.minimumZoomScale) {
             scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
         } else {
@@ -107,45 +115,21 @@ class ImageViewController : UIViewController, UIScrollViewDelegate {
         }
     }
     
-    func filterImage(completion: (Bool) -> ()) {
-        
-        print("Creating image")
-        
-        guard let image = imageView?.image, cgimg = image.CGImage else {
-            print("imageView doesn't have an image!")
-            completion(false)
-            return
-        }
-        
-        let openGLContext = EAGLContext(API: .OpenGLES2)
-        let context = CIContext(EAGLContext: openGLContext!)
-        let coreImage = CIImage(CGImage: cgimg)
-        
-        let sepiaFilter = CIFilter(name: "CISepiaTone")
-        sepiaFilter?.setValue(coreImage, forKey: kCIInputImageKey)
-        sepiaFilter?.setValue(1, forKey: kCIInputIntensityKey)
-        
-        if let sepiaOutput = sepiaFilter?.valueForKey(kCIOutputImageKey) as? CIImage {
-            let exposureFilter = CIFilter(name: "CIExposureAdjust")
-            exposureFilter?.setValue(sepiaOutput, forKey: kCIInputImageKey)
-            exposureFilter?.setValue(1, forKey: kCIInputEVKey)
-            print("CISepiaTone applied")
-            
-            if let exposureOutput = exposureFilter?.valueForKey(kCIOutputImageKey) as? CIImage {
-                let output = context.createCGImage(exposureOutput, fromRect: exposureOutput.extent)
-                let result = UIImage(CGImage: output)
-                print("CIExposureAdjust applied")
-
-                
-                NSOperationQueue.mainQueue().addOperationWithBlock({
-                    print("Setting the new image")
-                    self.imageView?.image = result
-
-                })
-                
-                print("Returning completion from createImage")
-                completion(true)
-            }
-        }
+    func setupViews() {
+        imageView = UIImageView(image: UIImage(named: "FlatironFam"))
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.blackColor()
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        scrollView.contentOffset = CGPoint(x: 800, y: 200)
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.delegate = self
+        activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+        activityIndicator.color = UIColor.cyanColor()
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
+        setZoomScale()
+        setupGestureRecognizer()
     }
 }
