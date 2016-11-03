ac192e0fb87804c6d65ab91f5e51d080ba504e00
ianrahman <ian.alexander.rahman@gmail.com>
2016-11-03 05:08:11 -0400

    Cleaned up for master

--- swift-multithreading-lab/ImageViewController.swift
+++ swift-multithreading-lab/ImageViewController.swift
@@ -22,7 +22,8 @@ class ImageViewController : UIViewController {
     let filtersToApply = ["CIBloom",
                           "CIPhotoEffectProcess",
                           "CIExposureAdjust"]
-    var flatigram = Flatigram()
+    
+//    var flatigram = Flatigram()
     
     @IBOutlet weak var filterButton: UIBarButtonItem!
     @IBOutlet weak var chooseImageButton: UIBarButtonItem!
@@ -39,67 +40,6 @@ class ImageViewController : UIViewController {
     
     @IBAction func filterButtonTapped(_ sender: AnyObject) {
         
-        switch (flatigram.state) {
-        case .unfiltered:
-            startProcess()
-        case .filtering:
-            print("nope")
-        case .filtered:
-            presentFilteredAlert()
-        }
-    }
-    
-}
-
-
-// MARK: Image Functions
-
-extension ImageViewController {
-    
-    func startProcess() {
-        
-        activityIndicator.startAnimating()
-        filterButton.isEnabled = false
-        chooseImageButton.isEnabled = false
-        
-        filterImage { result in
-            
-            result ? print("Image successfully filtered") : print("Image filtering did not complete")
-            self.imageView.image = self.flatigram.image
-            self.activityIndicator.stopAnimating()
-            self.filterButton.isEnabled = true
-            self.chooseImageButton.isEnabled = true
-        }
-    }
-    
-    func filterImage(with completion: @escaping (Bool) -> ()) {
-        
-        let queue = OperationQueue()
-        queue.name = "Image Filtration queue"
-        queue.maxConcurrentOperationCount = 1
-        queue.qualityOfService = .userInitiated
-        
-        for filter in filtersToApply {
-            
-            let filterer = FilterOperation(flatigram: flatigram, filter: filter)
-            filterer.completionBlock = {
-                
-                if filterer.isCancelled {
-                    completion(false)
-                    return
-                }
-                
-                if queue.operationCount == 0 {
-                    self.flatigram.state = .filtered
-                    completion(true)
-                }
-                
-
-            }
-            
-            queue.addOperation(filterer)
-            print("Added FilterOperation with \(filter) to \(queue.name!)")
-        }
     }
     
 }
